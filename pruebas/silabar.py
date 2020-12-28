9139fe42a8eb10d9093aee5f9fd4f63cd3541ab0
raytrapi <sdfenix@gmail.com>
2020-12-28 20:43:27 +0100

    Red Neuronal por silabas

---
+++ pruebas/silabar.py
@@ -0,0 +1,143 @@
+tiposCaracter=[
+    ['0','1','2','3','4','5','6','7','8','9'],
+    ['i','u'],
+    ['a','e','o'],
+    ['á','é','í','ó','ú'],
+    ['b','c','d','f','g','h','j','k','l','m','n','ñ','p','q','r','s','t','v','w','x','y','z']
+]
+def obtenerTipoCaracter(c):
+    for idx,tipo in enumerate(tiposCaracter):
+        if c in tipo:
+            return idx
+    return len(tiposCaracter)
+consonantesNoSeparables={
+    "b":"r", 
+    "c":"r", 
+    "d":"r", 
+    "g":"r", 
+    "f":"r", 
+    "k":"r", 
+    "t":"r", 
+    "b":"l", 
+    "c":"l", 
+    "g":"l", 
+    "f":"l", 
+    "k":"l", 
+    "p":"l",
+
+    "l":"l"
+    
+}
+longitudTiposCaracter=len(tiposCaracter)
+prefijos=["intro", "intra","micro", "multi", "sobre", "super", "abs","anti", "auto", "post", "hemi", "hipo", "meta", "mega", "omni", "tele", "pre", "pos", "sin","sub", "an", "bi", "co", "in", "im", "re"]
+sufijos=["arquía", "filia", "isimo", "cidio", "logía", "ultra", "audi", "ismo", "cida", "des", "dis", "ito", "ita", "azo","de", "di"]
+def obtenerSilabas(palabra):
+   silabas=[]
+   sinVocal=True
+   silaba=""
+   tipoCaracter=-1
+   #tipoUltimoCaracter=-1
+   #caracterAnterior=""
+   #caracteresGuardados=""
+   palabraMinuscula=palabra.lower()
+   separar=False
+   desde=0
+   hasta=len(palabra)
+   su_=[]
+   #quitamos los signos de puntuación
+   for i in range(len(palabraMinuscula)):
+      if obtenerTipoCaracter(palabraMinuscula[i])<longitudTiposCaracter:
+         break
+      silabas.append(palabra[i])
+      desde+=1
+   for i in range(len(palabraMinuscula)):
+      if obtenerTipoCaracter(palabraMinuscula[len(palabraMinuscula)-1-i])<longitudTiposCaracter:
+         break
+      su_.insert(0,palabra[len(palabraMinuscula)-1-i])
+      hasta-=1
+   palabraMinuscula=palabraMinuscula[desde:hasta]
+   palabra=palabra[desde:hasta]
+   desde=0
+   hasta=len(palabra)
+   #Primero buscamos los prefijos y los sufijos
+   
+   iAnterior=-1
+   iSiguiente=1
+   for p in prefijos:
+      if p==palabraMinuscula[0:len(p)]:
+         silabas.append(palabra[0:len(p)])
+         desde=len(p)
+         break
+   
+   for s in sufijos:
+      if s==palabraMinuscula[len(palabra)-len(s):]:
+         su_.insert(0,palabra[len(palabra)-len(s):])
+         hasta=len(palabra)-len(s)
+         break
+   #Casos especiales
+   #if desde==0 and len(palabraMinuscula)>3 and "amb" ==palabraMinuscula[0:3]:
+   #   desde=2
+   #   silabas.append(palabra[0:2])
+   palabraMinuscula=palabraMinuscula[desde:hasta]
+   palabra=palabra[desde:hasta]
+   ultimoIdxPalabra=len(palabra)-1 #Pongo 2 para evitar sumar más abajo en la 
+   for i in range(len(palabraMinuscula)):
+      c=palabra[i]
+      cMinuscula=palabraMinuscula[i]
+      
+      cAnterior=palabraMinuscula[iAnterior] if iAnterior>=0 else ""
+      cSiguiente=palabraMinuscula[iSiguiente] if iSiguiente<=ultimoIdxPalabra else ""
+      tipoCaracter=obtenerTipoCaracter(cMinuscula)
+      tipoCaracterIz=obtenerTipoCaracter(cAnterior)
+      tipoCaracterDe=obtenerTipoCaracter(cSiguiente)
+      
+      noSeparable=(cAnterior in consonantesNoSeparables and consonantesNoSeparables[cAnterior]==cMinuscula)
+      separar=(cMinuscula in consonantesNoSeparables and consonantesNoSeparables[cMinuscula]==cSiguiente) or tipoCaracter>4 or tipoCaracterIz>4
+      separar=separar or (cMinuscula == 't' and cAnterior=='s')
+      separar=separar or (cMinuscula == 'b' and cAnterior=='m')
+      junto=tipoCaracter==4 and (tipoCaracterIz in [1,2,3] and tipoCaracterDe not in [1,2,3]) and len(silaba)>1
+      junto=junto or (tipoCaracterIz in [1,2] and tipoCaracter in [1,2,3])or (tipoCaracterIz in [1,2,3] and tipoCaracter in [1,2])
+      #junto=junto or (tipoCaracter == "m" and cSiguiente=="b")
+      if silaba=="" or ((sinVocal or noSeparable or junto) and not separar):
+         if tipoCaracter in [1,2,3]:
+               sinVocal=False
+         silaba+=c
+      else:
+         if len(silaba)==1 and obtenerTipoCaracter(silaba)==4 and len(silabas)>0:
+            silabas[len(silabas)-1]+=silaba
+         else:
+            silabas.append(silaba)
+         silaba=c
+         #caracteresGuardados=""
+         sinVocal=tipoCaracter not in [1,2,3]
+      if not noSeparable:
+         iAnterior=i
+      #iAnterior=i
+      iSiguiente=i+2
+      #tipoUltimoCaracter=tipoCaracter
+      #caracterAnterior=c
+   if tipoCaracter==4 and len(silabas)>0 and len(silaba)==1:
+      silabas[len(silabas)-1]+=silaba
+   else:
+      silabas.append(silaba)
+   silabas=silabas+su_
+   silabas[len(silabas)-1]+="_"
+   return silabas
+
+'''
+palabrasPrueba=[
+    "aprender", "Abscisas", "usurpar","mesa", "silla", "comer","azul", "camisa","época",
+    "cofre", "ladrón", "plato", "francés","subrayar", "sublunar", "súbliminar","postromántico",
+    "cuota","ambiguo","cuidar","ruido","bueno","cuerpo","guardar","paraguas","ciudades",
+    "triunfo","violencia","violín","tiembla","siembra","caiga","vaina","gourmet","Lourdes",
+    "oigo","heroico","Eustaquio","reunión","reinar","aceite","autopista","caudal",
+    "baile","aire","Qué","ahuecar"
+]
+
+print(obtenerSilabas("¿aprender?"))
+print(obtenerSilabas("estás?"))
+for palabra in palabrasPrueba:
+    print(palabra)
+    print(obtenerSilabas(palabra))
+
+'''
---
+++ pruebas/vocabulario.py
@@ -0,0 +1,68 @@
+import silabar
+def insertarPalabra(palabra, palabras, posiciones, minuscula, palabrasInvalidas=["\n"]):
+    if len(palabra)==0 or palabra in palabrasInvalidas:
+        return
+    if minuscula:
+        palabra=palabra.lower()
+    if(palabra not in palabras):
+        palabras.append(palabra)
+    indice=palabras.index(palabra)
+    posiciones.append(indice)
+#Vocabulario obtiene tanto la lista de palabras como las posiciones por párrafo de cada palabra en el texto dado
+#juntarParrafos tiene en cuenta que un \n que no está precedido por un "." o un ". " no se considera como parrafo nuevo
+def vocabulario1(linea, caracterSeparacion=' ', caracteresPuntuacion=['!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\','\'',']','^','_','`','{','|','}','~','\t','\n','\r','¿','¡'], caracteresDescartados=['\r', '\t'], minuscula=True, palabras=['\n'], conParrafos=False, juntarParrafos=False, caracteresFinalesParrafo=['.','_']):
+    posiciones=[]
+    parrafo=[]
+    esPuntuacion=False
+    palabra=""
+    ultimoCaracter=""
+    palabrasInvalidas=caracteresDescartados
+    for c in linea:
+        if(c==caracterSeparacion):
+            #if(esPuntuacion):
+            #    palabra+=c
+            insertarPalabra(palabra, palabras, parrafo, minuscula,palabrasInvalidas)
+            palabra=""
+            esPuntuacion=False
+        else:
+            if(c not in caracteresPuntuacion):
+                if(esPuntuacion):
+                    insertarPalabra(palabra, palabras, parrafo,minuscula,palabrasInvalidas)
+                    esPuntuacion=False
+                    palabra=c
+                else:
+                    palabra+=c
+            else:
+                insertarPalabra(palabra, palabras, parrafo,minuscula,palabrasInvalidas)
+                palabra=""
+                if c not in caracteresDescartados:
+                    palabra=c
+                    esPuntuacion=True
+        if c!=caracterSeparacion and c not in caracteresDescartados and c!='\n':
+            ultimoCaracter=c
+    
+    if conParrafos and len(parrafo)>0:
+        if(not juntarParrafos or ultimoCaracter in caracteresFinalesParrafo):
+            posiciones.append(parrafo)
+            parrafo=[]
+    #TODO Implementar un mecanismo para evitar palabras cortadas al final de línea con el -
+    if palabra!="":
+        insertarPalabra(palabra, palabras, parrafo,minuscula,palabrasInvalidas)
+        palabra=""
+
+    if(len(parrafo)>0):
+        posiciones.append(parrafo)
+        parrafo=[]
+    return palabras,posiciones
+
+def vocabulario(frase, voc=[None,"_^_","_$_"]):
+   palabras=frase.split()
+   posiciones=[]
+   for palabra in palabras:
+      vocTemp=silabar.obtenerSilabas(palabra)
+      for v in vocTemp:
+         if v not in voc:
+            voc.append(v)
+         posiciones.append(voc.index(v))
+      
+   return voc,[posiciones]
